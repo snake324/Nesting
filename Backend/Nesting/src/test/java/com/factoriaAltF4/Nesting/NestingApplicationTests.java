f6dbed0a3a5ad0b438d191cf0b1bc9a68ec24676
DirtdiverIV <davidgomez.seg@gmail.com>
2023-09-04 11:06:44 +0200

    Created Porject Folders and SpringBoot Inicializr

---
+++ Backend/Nesting/src/test/java/com/factoriaAltF4/Nesting/NestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.factoriaAltF4.Nesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
