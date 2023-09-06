81082481e164b64330326f558a8d6f224a0eb88f
snake324 <kristi4n90@gmail.com>
2023-09-06 15:43:23 +0200

    admin modules and all routing

--- Frontend/nesting/src/app/app-routing.module.ts
+++ Frontend/nesting/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'properties', pathMatch: 'full' },
+  { path: 'properties', loadChildren: () => import('./properties/properties.module').then(m => m.PropertiesModule) },
+  { path: 'user-forms', loadChildren: () => import('./user-forms/user-forms.module').then(m => m.UserFormsModule) },
+  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
