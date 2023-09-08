9a7f0895d12efca35bc21db182a568481c9c7dce
snake324 <kristi4n90@gmail.com>
2023-09-08 16:02:53 +0200

    routes solved

--- Frontend/nesting/src/app/properties/properties.module.ts
+++ Frontend/nesting/src/app/properties/properties.module.ts
@@ -5,13 +5,28 @@ import { PropertiesRoutingModule } from './properties-routing.module';
 import { PropertyFormComponent } from './views/property-form/property-form.component';
 import { PrevisualizationComponent } from './views/previsualization/previsualization.component';
 import { FilterboxComponent } from './components/filterbox/filterbox.component';
+import { BuycardsComponent } from './components/buycards/buycards.component';
+import { HomecardsComponent } from './components/homecards/homecards.component';
+import { RentcardsComponent } from './components/rentcards/rentcards.component';
+import { BuyComponent } from './views/buy/buy.component';
+import { DetailsComponent } from './views/details/details.component';
+import { HomeComponent } from './views/home/home.component';
+import { RentComponent } from './views/rent/rent.component';
 
 
 @NgModule({
   declarations: [
     PropertyFormComponent,
     PrevisualizationComponent,
-    FilterboxComponent
+    BuyComponent,
+    DetailsComponent,
+    HomeComponent,
+    RentComponent,
+    FilterboxComponent,
+    BuycardsComponent,
+    HomecardsComponent,
+    RentcardsComponent,
+
   ],
   imports: [
     CommonModule,
