084db87c59bfcda154555de1cc04d65570360198
snake324 <kristi4n90@gmail.com>
2023-09-08 15:29:32 +0200

    component filterbox created

--- Frontend/nesting/src/app/properties/properties.module.ts
+++ Frontend/nesting/src/app/properties/properties.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { PropertiesRoutingModule } from './properties-routing.module';
 import { PropertyFormComponent } from './views/property-form/property-form.component';
 import { PrevisualizationComponent } from './views/previsualization/previsualization.component';
+import { FilterboxComponent } from './components/filterbox/filterbox.component';
 
 
 @NgModule({
   declarations: [
     PropertyFormComponent,
-    PrevisualizationComponent
+    PrevisualizationComponent,
+    FilterboxComponent
   ],
   imports: [
     CommonModule,
