8a9edc7d8bd93ea68ad1d22a35b9d7ba4dddd565
Fco Manuel <fsaa93@gmail.com>
2023-09-13 12:52:39 +0200

    Properties new method findByType(alquiler o venta)

--- Backend/Nesting/src/main/java/com/factoriaAltF4/Nesting/repositories/PropertyRepository.java
+++ Backend/Nesting/src/main/java/com/factoriaAltF4/Nesting/repositories/PropertyRepository.java
@@ -1,9 +1,16 @@
 package com.factoriaAltF4.Nesting.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.factoriaAltF4.Nesting.models.Property;
 
 public interface PropertyRepository extends JpaRepository<Property,Long>{
     
+    @Query("SELECT p from Property p WHERE p.type = :type")
+    Optional<List<Property>> findBytype(String type);
+
 }
