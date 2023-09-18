c425c09d6fc99e6a31cd96101f12b512d9ff1640
snake324 <kristi4n90@gmail.com>
2023-09-18 11:11:12 +0200

    user logged redirected to home

--- Frontend/nesting/src/app/user-forms/components/signin/signin.component.ts
+++ Frontend/nesting/src/app/user-forms/components/signin/signin.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { UserService } from '../../service/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
@@ -11,7 +12,7 @@ export class SigninComponent {
   email: string = '';
   password: string = '';
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private router: Router) {}
 
   onSubmit() {
     if (this.email && this.password) {
@@ -19,6 +20,7 @@ export class SigninComponent {
         .subscribe(
           (user) => {
             console.log('Inicio de sesión exitoso:', user);
+            this.router.navigate(['/properties/home']);
           },
           (error) => {
             console.error('Error al iniciar sesión:', error);
