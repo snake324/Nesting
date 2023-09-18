fcb4bcb71eeee5ed7a8a687610df1a2f91037ae9
snake324 <kristi4n90@gmail.com>
2023-09-18 15:03:04 +0200

    routing login working

--- Frontend/nesting/src/app/user-forms/user-forms-routing.module.ts
+++ Frontend/nesting/src/app/user-forms/user-forms-routing.module.ts
@@ -3,14 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './views/login/login.component';
 import { RegisterComponent } from './views/register/register.component';
 import { ProfileComponent } from './views/profile/profile.component';
-import { HomeComponent } from '../properties/views/home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
-  { path: 'profile', component: ProfileComponent},
-  { path: 'home', component: HomeComponent}
+  { path: 'profile', component: ProfileComponent}
 ];
 
 @NgModule({
