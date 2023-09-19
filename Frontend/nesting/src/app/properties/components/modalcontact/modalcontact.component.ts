4128a91244629926524954762bd81eb11c69a781
Guillermo <guillermo.llausas@gmail.com>
2023-09-19 12:18:31 +0200

    Changes on the styles of Modal module

--- Frontend/nesting/src/app/properties/components/modalcontact/modalcontact.component.ts
+++ Frontend/nesting/src/app/properties/components/modalcontact/modalcontact.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-modalcontact',
   templateUrl: './modalcontact.component.html',
-  styleUrls: ['./modalcontact.component.scss']
+  styleUrls: ['./modalcontact.component.scss'],
 })
 export class ModalcontactComponent {
-  isVisible = false;
+  isVisible: boolean = false;
 
   openModal() {
     this.isVisible = true;
