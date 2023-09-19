5bcb159e9aa54ba53868b3ac4ccafa12499b46aa
Guillermo <guillermo.llausas@gmail.com>
2023-09-19 12:47:10 +0200

    Delete of the HomeCards commentaries

--- Frontend/nesting/src/app/properties/components/modalcontact/modalcontact.component.ts
+++ Frontend/nesting/src/app/properties/components/modalcontact/modalcontact.component.ts
@@ -3,16 +3,20 @@ import { Component, ViewChild } from '@angular/core';
 @Component({
   selector: 'app-modalcontact',
   templateUrl: './modalcontact.component.html',
-  styleUrls: ['./modalcontact.component.scss'],
+  styleUrls: ['./modalcontact.component.scss']
 })
 export class ModalcontactComponent {
   isVisible: boolean = false;
 
-  openModal() {
-    this.isVisible = true;
-  }
+  
 
-  closeModal() {
-    this.isVisible = false;
-  }
+   openModal() {
+     this.isVisible = true;
+ }
+
+
+   closeModal() {
+   this.isVisible = false;
+ }
+
 }
