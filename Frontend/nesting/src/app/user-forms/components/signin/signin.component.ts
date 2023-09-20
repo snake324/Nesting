486e859f6849e40f6baeaeac8ca33e1e1127d035
snake324 <kristi4n90@gmail.com>
2023-09-20 14:11:33 +0200

    login working, but doesnt generate cookie

--- Frontend/nesting/src/app/user-forms/components/signin/signin.component.ts
+++ Frontend/nesting/src/app/user-forms/components/signin/signin.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { UserService } from '../../service/user.service';
-import { Router } from '@angular/router';
+import { HttpHeaders } from '@angular/common/http';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
@@ -9,23 +10,46 @@ import { Router } from '@angular/router';
 })
 export class SigninComponent {
 
-  email: string = '';
-  password: string = '';
+  formlogin!: FormGroup;
+  submitted = false;
+  loading = false;
+  errorMessage: string | null = null; // Variable para mostrar mensajes de error
+  
+  constructor(private usersService: UserService, private formBuilder: FormBuilder) {}
 
-  constructor(private userService: UserService, private router: Router) {}
-
-  onSubmit() {
-    if (this.email && this.password) {
-      this.userService.login(this.email, this.password)
-        .subscribe(
-          (user) => {
-            console.log('Inicio de sesión exitoso:', user);
-            this.router.navigate(['/properties/home']);
-          },
-          (error) => {
-            console.error('Error al iniciar sesión:', error);
-          }
-        );
-    }
+  ngOnInit() {
+    this.formlogin = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4)]]
+    });
   }
-}
+
+  login() {
+    this.errorMessage = null;
+  
+    if (this.formlogin.invalid) {
+      return;
+    }
+  
+    const username = this.formlogin.value.username;
+    const password = this.formlogin.value.password;
+  
+    const authHeader = 'Basic ' + btoa(username + ':' + password);
+    const headers = new HttpHeaders({ 'Authorization': authHeader });
+  
+    this.usersService.loginUser(username, password, headers).subscribe(
+      (data) => {
+        console.log(data);
+      },
+      (error) => {
+        console.error('Login error:', error);
+        if (error.status === 401) {
+          this.errorMessage = 'Credenciales incorrectas. Por favor, verifica tus datos.';
+        } else {
+          this.errorMessage = 'Hubo un error en el inicio de sesión. Por favor, intenta de nuevo más tarde.';
+        }
+      }
+    );
+  }
+  }
+  
