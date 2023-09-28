d2c798e8c2865482945863397fdba909093ef3b1
snake324 <kristi4n90@gmail.com>
2023-09-28 11:34:48 +0200

    utf8 fixed

--- Frontend/nesting/src/app/global/header/header.component.ts
+++ Frontend/nesting/src/app/global/header/header.component.ts
@@ -14,6 +14,10 @@ export class HeaderComponent {
     private userService: UserService
     ) {}
 
+    ngOnInit() {
+    }
+    
+
   isProfilePage(): boolean {
     return this.router.url.startsWith('/user-forms/profile/');
   }
@@ -28,6 +32,15 @@ export class HeaderComponent {
         console.error('Error durante el logout:', error);
       }
     );
+  }
+  
+  navigateToProfileOrLogin() {
+    const user = this.userService.isUserAuthenticated();
+    if (user) {
+      this.router.navigate(['/user-forms/profile', user.iduser]);
+    } else {
+      this.router.navigate(['/user-forms/login']);
+    }
   }
   
 }
--- Frontend/nesting/src/app/user-forms/service/user.service.ts
+++ Frontend/nesting/src/app/user-forms/service/user.service.ts
@@ -11,8 +11,10 @@ export class UserService {
   private apiUrl = 'http://localhost:4000/login';
   private baseUrl = 'http://localhost:4000';
 
-  private actualUser!: User;
+  private isAuthenticated: boolean = false;
+  private actualUser: User | null = null;
   constructor(private httpClient: HttpClient) {}
+
   public registerUser(mail: string, password: string): Observable<any> {
     const status = true;
 
@@ -25,7 +27,12 @@ export class UserService {
     return this.httpClient.post<any>(`${this.baseUrl}/register`, user);
   }
   public loginUser(mail: string, password: string, headers: HttpHeaders): Observable<any> {
+    this.isAuthenticated = true;
     return this.httpClient.post<any>(`${this.baseUrl}/login`, {}, { headers, withCredentials: true  });
+  }
+
+  public isUserAuthenticated(): User | null {
+    return this.isAuthenticated ? this.actualUser : null;
   }
 
   setUser(user: any) {
@@ -42,6 +49,8 @@ export class UserService {
   }
 
   public logout(): Observable<any> {
+    this.actualUser = null;
+    this.isAuthenticated = false;
     return this.httpClient.post<any>(`${this.baseUrl}/logout`, {}, { withCredentials: true });
   }
 }
