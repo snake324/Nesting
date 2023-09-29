98b06507a9334d9da1d966cd21fbafbc275b8d23
snake324 <kristi4n90@gmail.com>
2023-09-29 09:19:51 +0200

    add card working no DB

--- Frontend/nesting/src/app/user-forms/components/card-added/card-added.component.ts
+++ Frontend/nesting/src/app/user-forms/components/card-added/card-added.component.ts
@@ -7,14 +7,31 @@ import { CardService } from '../../service/card.service';
   styleUrls: ['./card-added.component.scss']
 })
 export class CardAddedComponent implements OnInit {
-  @Input() cardData: any = {};
+  @Input() 
+  cards: any[] = [];
 
   constructor(private cardService: CardService) {}
 
   ngOnInit() {
-    this.cardService.cardSavedSuccessfully$.subscribe((data) => {
-      this.cardData = data; 
-      console.log('cardData received:', this.cardData);
+    this.cardService.cardSavedSuccessfully$.subscribe(() => {
+      this.cards = this.cardService.cards;
+  
+      console.log('cardData received:', this.cards);
     });
   }
+  
+  formatCardNumber(cardNumber: string): string {
+    // Elimina espacios en blanco y caracteres no numéricos
+    const cleanedNumber = cardNumber.replace(/\D/g, '');
+    
+    // Divide el número en grupos de 4 cifras
+    const parts = [];
+    for (let i = 0; i < cleanedNumber.length; i += 4) {
+      parts.push(cleanedNumber.substring(i, i + 4));
+    }
+    
+    // Une los grupos con espacios
+    return parts.join(' ');
+  }
+  
 }
--- Frontend/nesting/src/app/user-forms/service/card.service.ts
+++ Frontend/nesting/src/app/user-forms/service/card.service.ts
@@ -5,9 +5,13 @@ import { Injectable, EventEmitter } from '@angular/core';
 })
 export class CardService {
   cardSavedSuccessfully$: EventEmitter<void> = new EventEmitter<void>();
-  cards: any[] = []; 
+  cards: any[] = [];
 
   cardSavedSuccessfully() {
+    // Aquí, puedes realizar cualquier lógica necesaria, pero no es necesario pasar datos como argumento.
+    console.log('Card saved successfully'); // Por ejemplo, puedes imprimir un mensaje.
+    
+    // Después de guardar los datos, emite el evento para notificar que la tarjeta se guardó correctamente.
     this.cardSavedSuccessfully$.emit();
   }
 
@@ -15,7 +19,11 @@ export class CardService {
     // Aquí deberías implementar la lógica para guardar los datos de la tarjeta,
     // por ejemplo, almacenarlos en un arreglo o en una base de datos.
     console.log('Received card data:', data);
-    // Después de guardar los datos, emite el evento para notificar que la tarjeta se guardó correctamente.
-    this.cardSavedSuccessfully$.emit();
+  
+    // Guarda los datos de la tarjeta en el arreglo de tarjetas
+    this.cards.push(data);
+  
+    // Llama al método cardSavedSuccessfully después de guardar los datos.
+    this.cardSavedSuccessfully();
   }
 }
