338be5a5e630de6a8f71206e63724c6aff27bc6c
María <133371373+Mariafernandezsantos@users.noreply.github.com>
2023-10-02 09:53:22 +0200

    ...

--- Frontend/nesting/src/app/properties/components/sale-rent/sale-rent.component.ts
+++ Frontend/nesting/src/app/properties/components/sale-rent/sale-rent.component.ts
@@ -1,30 +1,24 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
-
-
 @Component({
   selector: 'app-sale-rent',
   templateUrl: './sale-rent.component.html',
-  styleUrls: ['./sale-rent.component.scss']
+  styleUrls: ['./sale-rent.component.scss'],
 })
-export class SaleRentComponent  {
-  
-  
+export class SaleRentComponent {
   propertyForm: FormGroup;
 
-  
   cities = [
     { name: 'Madrid', postalCodes: ['28001', '28002', '28003'] },
     { name: 'Barcelona', postalCodes: ['08001', '08002', '08003'] },
     { name: 'Valencia', postalCodes: ['46001', '46002', '46003'] },
-    { name: 'Oviedo', postalCodes: ['33001', '33002', '33003'] }
+    { name: 'Oviedo', postalCodes: ['33001', '33002', '33003'] },
   ];
 
   filteredPostalCodes: string[] = [];
 
   constructor(private formBuilder: FormBuilder) {
-    
     this.propertyForm = this.formBuilder.group({
       propertyType: [''],
       transactionType: [''],
@@ -34,15 +28,30 @@ export class SaleRentComponent  {
       postalCode: [''],
       surface: [''],
       price: [''],
-      description: ['']
+      description: [''],
     });
   }
 
-  
+  selectPropertyType(propertyType: string) {
+    this.propertyForm.patchValue({ propertyType });
+  }
+
+  selectTransactionType(transactionType: string) {
+    this.propertyForm.patchValue({ transactionType });
+  }
+
+  selectCity(city: string) {
+    this.propertyForm.patchValue({ city });
+    this.onCitySelect();
+  }
+
+  selectPostalCode(postalCode: string) {
+    this.propertyForm.patchValue({ postalCode });
+  }
+
   onCitySelect() {
-    console.log('onCitySelect called');
     const selectedCity = this.propertyForm.get('city')?.value;
-    const city = this.cities.find(c => c.name === selectedCity);
+    const city = this.cities.find((c) => c.name === selectedCity);
 
     if (city) {
       this.filteredPostalCodes = city.postalCodes;
@@ -51,18 +60,11 @@ export class SaleRentComponent  {
     }
 
     const isDisabled = !selectedCity;
-    console.log('Disabled:', isDisabled);
-
-  
     this.propertyForm.patchValue({ postalCode: '' });
   }
 
-  
   onSubmit() {
     const formData = this.propertyForm.value;
-    
+    console.log('Form Data:', formData);
   }
-
- 
-
 }
