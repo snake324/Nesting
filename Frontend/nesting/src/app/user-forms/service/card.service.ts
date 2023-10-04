874795b0c1e9aec60a4bb79326ac2e24920558c8
snake324 <kristi4n90@gmail.com>
2023-10-04 14:01:13 +0200

    update

--- Frontend/nesting/src/app/user-forms/service/card.service.ts
+++ Frontend/nesting/src/app/user-forms/service/card.service.ts
@@ -1,11 +1,12 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CardService {
-  private apiUrl = 'http://localhost:4000/cards'; // Cambia la URL según tu configuración de backend
+  private apiUrl = 'http://localhost:4000/cards';
 
   cardSavedSuccessfully$: EventEmitter<void> = new EventEmitter<void>();
   cards: any[] = [];
@@ -13,29 +14,35 @@ export class CardService {
   constructor(private http: HttpClient) {}
 
   cardSavedSuccessfully() {
-    // Aquí, puedes realizar cualquier lógica necesaria, pero no es necesario pasar datos como argumento.
-    console.log('Card saved successfully'); // Por ejemplo, puedes imprimir un mensaje.
+    console.log('Card saved successfully');
     
-    // Después de guardar los datos, emite el evento para notificar que la tarjeta se guardó correctamente.
     this.cardSavedSuccessfully$.emit();
   }
 
   saveCardData(data: any) {
-    // Aquí deberías implementar la lógica para guardar los datos de la tarjeta en el backend.
-    // Puedes usar HttpClient para enviar una solicitud POST al endpoint /create.
     console.log('Sending card data to backend:', data);
 
     this.http.post(`${this.apiUrl}/create`, data).subscribe(
       (response) => {
         console.log('Card saved successfully in the backend:', response);
-        // Llama al método cardSavedSuccessfully después de guardar los datos en el backend.
         this.cardSavedSuccessfully();
       },
       (error) => {
         console.error('Error saving card data in the backend:', error);
-        // Aquí puedes manejar errores si es necesario.
       }
     );
+  }
+
+  getAllCards(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  getCardById(cardId: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}/${cardId}`);
+  }
+
+  deleteCard(cardId: string): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/delete/${cardId}`);
   }
 }
 
