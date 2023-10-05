889614a60a1e1c2c8969f90576ade3efaf8e2637
snake324 <kristi4n90@gmail.com>
2023-10-05 12:55:46 +0200

    update

--- Backend/Nesting/src/main/java/com/factoriaAltF4/Nesting/models/UserProfile.java
+++ Backend/Nesting/src/main/java/com/factoriaAltF4/Nesting/models/UserProfile.java
@@ -32,7 +32,7 @@ public class UserProfile {
     @JsonIgnore
     private User user;
 
-    @OneToOne(mappedBy = "userProfile")
+    @OneToOne(mappedBy = "userProfile", cascade = CascadeType.ALL)
     public Card card;
 
     // @ManyToMany(mappedBy = "userProfiles")
