77d25ec6a6ae4565fb6fdf3ca13cc04f12056e54
snake324 <kristi4n90@gmail.com>
2023-10-06 09:02:57 +0200

    images loading

--- Frontend/nesting/src/app/properties/components/file-upload/file-upload.component.ts
+++ Frontend/nesting/src/app/properties/components/file-upload/file-upload.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Image } from '../../models/image.model';
 
 @Component({
   selector: 'app-file-upload',
@@ -6,7 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./file-upload.component.scss']
 })
 export class FileUploadComponent {
-uploadedImages: any;
-deleteImage: any;
 
+  @ViewChild('imageInput', { static: false }) imageInputRef: ElementRef | undefined;
+
+  imageUrls: string[] = [];
+
+  onFileImageClick(): void {
+    const fileInput: HTMLInputElement | null = this.imageInputRef?.nativeElement;
+    if (fileInput) {
+      fileInput.click();
+    }
+  }
+
+  onFilesSelected(event: any) {
+    const files: FileList | null = event.target.files;
+    if (files && files.length > 0) {
+      this.imageUrls = [];
+      for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+        this.imageUrls.push(this.getObjectURL(file));
+      }
+    }
+  }
+
+  getObjectURL(file: File | null): string {
+    if (file) {
+      return URL.createObjectURL(file);
+    }
+    return '';
+  }
 }
